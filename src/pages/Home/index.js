41f35dfe3139499e3b3ed8dfe83eca96c8884669
AtilioA <atiliodadalto@hotmail.com>
2020-06-04 21:37:20 -0300

    Consume fake API

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,91 +1,51 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md'
 import { ProductList } from './styles';
+import api from '../../services/api';
+import { formatPrice } from '../../utils/format';
 
-function Home() {
-  return (
-    <ProductList>
-      <li>
-        <img src={require("../../assets/images/sneakers/Air Jordan.png")} alt="Air Jordan Sneakers" />
-        <strong>Nike Air Jordan 1 Retro Satin</strong>
-        <span>R$999,90</span>
+class Home extends Component {
+  state = {
+    products: [],
+  };
 
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
+  async componentDidMount() {
+    const response = await api.get('products');
+
+    const data = response.data.map(product => ({
+      ...product,
+      formattedPrice: formatPrice(product.price),
+    }));
+
+    this.setState({ products: data })
+  }
+
+  render() {
+    const { products } = this.state;
+    console.log(products)
+
+    return (
+      <ProductList>
+        {products.map(product => (
+          <li key={product.id}>
+            <img src={require('../../assets/images/sneakers/Air Jordan.png')} alt={product.title} />
+
+            <strong>{product.title}</strong>
+            <span>{product.formattedPrice}</span>
+
+            <button type="button">
+              <div>
+                <MdAddShoppingCart size={16} color="#FFF" /> 3
           </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
+              <span>Add to cart</span>
+            </button>
+          </li>
+        ))}
 
-      <li>
-        <img src={require("../../assets/images/sneakers/Air Jordan.png")} alt="Air Jordan Sneakers" />
-        <strong>Nike Air Jordan 1 Retro Satin</strong>
-        <span>R$999,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-
-      <li>
-        <img src={require("../../assets/images/sneakers/Air Jordan.png")} alt="Air Jordan Sneakers" />
-        <strong>Nike Air Jordan 1 Retro Satin</strong>
-        <span>R$999,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-
-      <li>
-        <img src={require("../../assets/images/sneakers/Air Jordan.png")} alt="Air Jordan Sneakers" />
-        <strong>Nike Air Jordan 1 Retro Satin</strong>
-        <span>R$999,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-
-      <li>
-        <img src={require("../../assets/images/sneakers/Air Jordan.png")} alt="Air Jordan Sneakers" />
-        <strong>Nike Air Jordan 1 Retro Satin</strong>
-        <span>R$999,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-
-      <li>
-        <img src={require("../../assets/images/sneakers/Air Jordan.png")} alt="Air Jordan Sneakers" />
-        <strong>Nike Air Jordan 1 Retro Satin</strong>
-        <span>R$999,90</span>
-
-        <button type="button">
-          <div>
-            <MdAddShoppingCart size={16} color="#FFF" /> 3
-          </div>
-          <span>Add to cart</span>
-        </button>
-      </li>
-
-    </ProductList>
-  );
+      </ProductList>
+    );
+  }
 }
 
 export default Home;
