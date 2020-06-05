40223d7ab5e9c50d15578efdd4098495c39123a6
AtilioA <atiliodadalto@hotmail.com>
2020-06-04 21:16:57 -0300

    Create Cart page and style

--- src/pages/Cart/index.js
+++ src/pages/Cart/index.js
@@ -1,9 +1,70 @@
 import React from 'react';
 
-// import { Container } from './styles';
+import { MdRemoveCircleOutline, MdAddCircleOutline, MdDelete } from 'react-icons/md'
+
+import { Container, ProductTable, Total } from './styles';
 
 function Cart() {
-  return <h1>Cart</h1>;
+  return (<Container>
+    <ProductTable>
+      <thead>
+        <tr>
+          <th />
+          <th>PRODUCT</th>
+          <th>QUANTITY</th>
+          <th>SUBTOTAL</th>
+          <th />
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>
+            <img src={require("../../assets/images/sneakers/Air Jordan.png")} alt="Air Jordan Sneakers" />
+          </td>
+
+          <td>
+            <strong>Nike Air Jordan 1 Retro Satin</strong>
+            <span>R$999,90</span>
+          </td>
+
+          <td>
+            <div>
+              <button type="button">
+                <MdRemoveCircleOutline size={20} color="#7159c1" />
+              </button>
+
+              <input type="number" readOnly value={3} />
+
+              <button type="button">
+                <MdAddCircleOutline size={20} color="#7159c1" />
+              </button>
+            </div>
+          </td>
+
+          <td>
+            <strong>R$2999,70</strong>
+          </td>
+
+          <td>
+            <button type="button">
+              <MdDelete size={20} color="#7159c1" />
+            </button>
+          </td>
+
+        </tr>
+      </tbody>
+    </ProductTable>
+
+
+    <footer>
+      <button type="button">Go to checkout</button>
+
+      <Total>
+        <span>TOTAL</span>
+        <strong>R$2999,70</strong>
+      </Total>
+    </footer>
+  </Container>);
 }
 
 export default Cart;
