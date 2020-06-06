3d8467e991e49d29f586075e86b882a22af327e3
AtilioA <atiliodadalto@hotmail.com>
2020-06-06 09:11:49 -0300

    Fix eslint dotfile and dependencies
    Add redux, react/redux

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,27 +1,28 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true
-    },
-    "extends": ["eslint:recommended", "prettier", "prettier/react", "plugin:react/recommended"],
-    "plugins": ["react", "prettier"],
-    "globals": {
-        "Atomics": "readonly",
-        "SharedArrayBuffer": "readonly"
-    },
-    "parser": "babel-eslint",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module",
-        "parser": "babel-eslint"
-    },
-    "rules": {
-        "prettier/prettier": "error",
-        "react/jsx-filename-extension": ["warn", { "extensions": [".jsx", ".js"] }],
-        "semi": ["error", "always"],
-        "class-methods-use-this": "off",
-        "no-param-reassign": "off",
-        "camelcase": "off",
-        "no-unused-vars": ["error", { "argsIgnorePattern": "next" }]
-    }
+  "env": {
+    "browser": true,
+    "es6": true
+  },
+  "extends": ["eslint:recommended", "prettier", "prettier/react", "plugin:react/recommended", "prettier/react"],
+  "plugins": ["react", "prettier"],
+  "globals": {
+    "Atomics": "readonly",
+    "SharedArrayBuffer": "readonly"
+  },
+  "parser": "babel-eslint",
+  "parserOptions": {
+    "ecmaVersion": 2018,
+    "sourceType": "module",
+    "parser": "babel-eslint"
+  },
+  "rules": {
+    "prettier/prettier": "error",
+    "react/jsx-filename-extension": ["warn", { "extensions": [".jsx", ".js"] }],
+    "semi": ["error", "always"],
+    "class-methods-use-this": "off",
+    "no-param-reassign": "off",
+    "camelcase": "off",
+    "no-unused-vars": ["error", { "argsIgnorePattern": "next" }],
+    'import/prefer-default-export': 'off',
+  }
 };
